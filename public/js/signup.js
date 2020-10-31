364b2ae9571c555695189c78be12f1a310160ddf
Kelly Kim <kellykim408@gmail.com>
2020-10-30 21:16:42 -0700

    update event listener for buttons

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,20 @@
  // direct to the new profile set up page when user clicks on signup button ========================
  $("#signupBtn").on("click", function (event) {
     event.preventDefault();
-    var email = $("#signEmail").val();
-    console.log(email);
-    var password = $("#password1").val();
-    console.log(password)
-    var password2 = $("#confirmPassword").val();
-    console.log(password2)
-    // redirect to new profile set-up page
-    window.location = "profileSetup.html";
+   
+   
+    if ($("#password1").val() === $("#confirmPassword").val()) {
+        var email = $("#signEmail").val();
+        console.log(email);
+        var password = $("#password1").val();
+        console.log(password)
+        var password2 = $("#confirmPassword").val();
+        console.log(password2)
+        // redirect to new profile set-up page
+        window.location = "profileSetup.html";
+    } else {
+        alert("Passwords don't match. Please try again.");
+    };
 })
    // direct to login page when user clicks on login button ========================
    $("#loginBtn").on("click", function (event) {
@@ -16,17 +22,4 @@
     // redirect to next user profile page
     window.location = "login.html";
 })
-// event listener for signup button ==========================================
-$("#signupBtn").on("click", function (event) {
-    // if no email is specified, do not make call and let user know
-    if ($("#email").val() === '') {
-        $("#email").addClass("is-danger");
-        if ($("#password").children().length === 7) {
-            $("#password").append($("<p>", { "class": "is-danger help", id: "removeMe", text: "Please input a password with atleast 8 characters." }))
-        }
-    } else {
-        // clear user input
-        $("#email").val('');
-        $("#password").val('');
-    }
-})
+
