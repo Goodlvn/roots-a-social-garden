068e4a97a4989230b5e6ed29ef12a1b772d3ca79
unknown <christy.g.lee@gmail.com>
2020-10-31 18:48:25 -0700

    started/created tags & attr for plant posts

---
+++ public/js/renderPlantPost.js
@@ -0,0 +1,47 @@
+let container = $("<article>");
+container.attr("class", "media");
+let figure = $("<figure>");
+figure.attr("class", "media-left");
+let imgCont = $("<p>");
+imgCont.attr("class", "image is-64x64");
+let img = $("<img>");
+img.attr("class", "https://versions.bulma.io/0.7.4/images/placeholders/128x128.png");
+
+let content = $("<div>");
+content.attr("class", "media-content");
+let postContent = $("<div>");
+postContent.attr("class", "content");
+
+
+let post = $("<p>");
+let strong = $("<strong>");
+let small = $("<small>");
+let strong = $("<strong>");
+let br = $("<br>");
+
+
+
+// let navLeft = $("<nav>");
+// nav.attr("class", "level is-mobile");
+// let icons = $("<div>");
+// icons.attr("class", "level-left");
+// let icon1 = $("<a>");
+// icon1.attr("class", "level-item");
+// let iconSpan = $("<span>");
+// iconSpan.attr("class", "icon is-small");
+
+// let replyIcon = $("<i>");
+// replyIcon.attr("class", "fas fa-reply");
+
+// let retweetIcon = $("<i>");
+// retweetIcon.attr("class", "fas fa-retweet");
+
+// let heartIcon = $("<i>");
+// heartIcon.attr("class", "fas fa-heart");
+
+// let navRight = $("<div>");
+// navRight.attr("class", "media-right");
+// let deleteIcon = $("<button>");
+// deleteIcon.attr("class", "delete");
+
+
