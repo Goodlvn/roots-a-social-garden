e7e5598ea1d111724550b100f4fcfb3871df8245
JD Martinez <focus4ursoul@gmail.com>
2020-10-31 18:38:57 -0700

    add passport middleware - user authentification

--- models/user.js
+++ models/user.js
@@ -3,28 +3,28 @@
 module.exports = (sequelize, DataTypes) => {
 
     const User = sequelize.define("user", {
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                len: [2]
-            }
-        },
+        // first_name: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // },
+        // last_name: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // },
+        // username: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     unique: true,
+        //     validate: {
+        //         len: [2]
+        //     }
+        // },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -36,14 +36,14 @@ module.exports = (sequelize, DataTypes) => {
         password: {
             type: DataTypes.STRING,
             allowNull: false
-        },
-        bio: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [10, 140],
-            }
         }
+        // bio: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [10, 140],
+        //     }
+        // }
     });
 
     User.associate = (models) => {
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require ("./config/passport");
 
 
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public"));
+
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // will need one of these for every routes file that we use
 require("./routes/html-routes.js")(app);
@@ -16,7 +21,6 @@ require("./routes/html-routes.js")(app);
 db.sequelize.sync(({ force: true })).then(() => {
     app.listen(PORT, () => {
         console.log("App listening on PORT: http://localhost:" + PORT);
-        // console.log(db.comment);
     });
 });
 
