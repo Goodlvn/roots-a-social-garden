f9732bea9e29976ae90799ba852b8b347cbef4be
JD Martinez <focus4ursoul@gmail.com>
2020-11-02 09:50:36 -0800

    re-adjust post model and comment box

--- public/js/renderPlantPost.js
+++ public/js/renderPlantPost.js
@@ -35,10 +35,8 @@ $(document).ready(() => {
 
 
                 for(i = data.length - 1; i >= 0; i--) {
-
                     let postNcom = $("<div>");
                     postNcom.attr("class", "full")
-
 
                     let postContainer = $("<article>");
                     postContainer.attr("class", "media full p-4");
@@ -57,12 +55,12 @@ $(document).ready(() => {
 
                     let content = $("<p>");
                     content.text(data[i].body);
+                    content.attr("class", "pb-3")
                     let userName = $("<strong>");
                     userName.text(fullName + " ");
                     let userHandle = $("<small>");
                     userHandle.text(handle);
                     let lineBreak = $("<br>");
-
 
                     $("#mainContainer").append(postNcom);
                     
@@ -70,7 +68,6 @@ $(document).ready(() => {
                     postContainer.append(profileImg);
                     profileImg.append(imgCont);
                     imgCont.append(img);
-
 
                     postContainer.append(postContent);
                     postContent.append(contentWrapper);
@@ -80,16 +77,14 @@ $(document).ready(() => {
                     content.prepend(userHandle);
                     content.prepend(userName);
 
-
                     let comContainer = $("<article>");
-                    comContainer.attr("class", "media full mt-6 pb-4 hide");
+                    comContainer.attr("class", "media full mt-4 pb-4 hide");
                     let profileImgCom = $("<figure>");
                     profileImgCom.attr("class", "media-left");
                     let imgContCom = $("<p>");
                     imgContCom.attr("class", "image is-64x64");
                     let imgCom = $("<img>");
                     imgCom.attr("src", "../images/jdsface.png");
-
 
                     let comContent = $("<div>");
                     comContent.attr("class", "media-content");
@@ -113,8 +108,6 @@ $(document).ready(() => {
                     submit.attr("class", "button is-info block");
                     submit.text("Submit");
 
-
-
                     postNcom.append(comContainer);
                     comContainer.append(profileImgCom);
                     profileImgCom.append(imgContCom);
@@ -124,7 +117,6 @@ $(document).ready(() => {
                     comContent.append(comWrapper);
                     comWrapper.append(comControl);
                     comControl.append(comText);
-
 
                     comContainer.append(lineBreakCom);
                     
@@ -141,16 +133,12 @@ $(document).ready(() => {
                     commentTag.attr("id", "commentTag");
                     commentTag.text("comments");
 
-
                     let line = $("<hr>");
                     line.attr("class", "full m-0")
-
 
                     postContainer.append(commentIcon);
                     postContainer.append(commentTag);
                     $("#mainContainer").append(line);
-                   
-
                 };
 
             });
