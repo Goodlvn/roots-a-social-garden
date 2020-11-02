749858d3587e959ffd000b9e56252b3de5b47876
unknown <christy.g.lee@gmail.com>
2020-11-02 00:41:47 -0800

    plantFinder model hide, buttone on click & url

---
+++ public/js/plantFinder.js
@@ -0,0 +1,14 @@
+$("#modal").hide();
+
+$("#plantFinderBtn").on("click", function(event){
+event.preventDefault();
+// $("#instructions").empty();
+// $("#plantName").text("Plant Name:")
+// $("#familyName").text("Family Common Name:")
+// $("#scientificName").text("Scientific Name:")
+// var plant = $("#searchPlant").val();
+var queryURL = "https://trefle.io/api/v1/plants/search?token=sAe0dDZyCrVdJe9k1ePcwrjgedscCASoJiMBAh3wRic&q=coconut";
+
+
+
+})
