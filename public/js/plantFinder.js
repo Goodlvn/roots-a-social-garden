08a74983de97724be84152fd26f5dc5f74c9ca11
unknown <christy.g.lee@gmail.com>
2020-11-02 00:52:14 -0800

    minor text changes lines 26-29

--- public/js/plantFinder.js
+++ public/js/plantFinder.js
@@ -2,11 +2,11 @@ $("#modal").hide();
 
 $("#plantFinderBtn").on("click", function(event){
 event.preventDefault();
-// $("#instructions").empty();
+$("#instructions").empty();
 // $("#plantName").text("Plant Name:")
 // $("#familyName").text("Family Common Name:")
 // $("#scientificName").text("Scientific Name:")
-// var plant = $("#searchPlant").val();
+var plant = $("#searchPlant").val();
 var queryURL = "https://trefle.io/api/v1/plants/search?token=sAe0dDZyCrVdJe9k1ePcwrjgedscCASoJiMBAh3wRic&q=coconut";
 $.ajax({
     url: queryURL,
@@ -24,9 +24,9 @@ $.ajax({
           var image = response.data[0].image_url;
         //   var x = response.data[0]
           $("#cardImage").attr("src", image)
-          $("#plantName").text(plantName);
-          $("#familyName").text(familyName);
-          $("#scientificName").text(scientificName);
+          $("#plantName").text("Plant Name: " + plantName);
+          $("#familyName").text("Family Common Name: " + familyName);
+          $("#scientificName").text("Scientific Name: " + scientificName);
 
           // var ingArray = [response.data[0].strIngredient1,response.drinks[0].strIngredient2,response.drinks[0].strIngredient3,response.drinks[0].strIngredient4,response.drinks[0].strIngredient5,response.drinks[0].strIngredient6,response.drinks[0].strIngredient7,response.drinks[0].strIngredient8,response.drinks[0].strIngredient9,response.drinks[0].strIngredient10]
           // var qtyArray = [x.strMeasure1,x.strMeasure2,x.strMeasure3,x.strMeasure4,x.strMeasure5,x.strMeasure6,x.strMeasure7,x.strMeasure8,x.strMeasure9,x.strMeasure10,]
