0ee599b50c51526549b526c2121c42637e28c62c
Kelly Kim <kellykim408@gmail.com>
2020-11-02 18:08:06 -0800

    create modals for circle  highlights

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -16,6 +16,77 @@ $(document).ready(function () {
        $("#plantPostModal").removeClass("is-active");  
     });
    
+// view circle images inside garden highlights ===================================
+//1st circle image
+ $("#plant1").on("click", function () {
+   $("#plant1Modal").addClass("is-active");
+});
+
+$("#plant1Modal").on("click", "#close", function () {
+   $("#plant1Modal").removeClass("is-active");  
+});
+
+$("#plant1Modal").on("click", ".modal-background", function () {
+   $("#plant1Modal").removeClass("is-active");  
+});
+
+//2nd circle image
+$("#plant2").on("click", function () {
+   $("#plant2Modal").addClass("is-active");
+});
+
+$("#plant2Modal").on("click", "#close", function () {
+   $("#plant2Modal").removeClass("is-active");  
+});
+
+$("#plant2Modal").on("click", ".modal-background", function () {
+   $("#plant2Modal").removeClass("is-active");  
+});
+
+//3nd circle image
+$("#plant3").on("click", function () {
+   $("#plant3Modal").addClass("is-active");
+});
+
+$("#plant3Modal").on("click", "#close", function () {
+   $("#plant3Modal").removeClass("is-active");  
+});
+
+$("#plant3Modal").on("click", ".modal-background", function () {
+   $("#plant3Modal").removeClass("is-active");  
+});
+
+//4th circle image
+$("#plant4").on("click", function () {
+   $("#plant4Modal").addClass("is-active");
+});
+
+$("#plant4Modal").on("click", "#close", function () {
+   $("#plant4Modal").removeClass("is-active");  
+});
+
+$("#plant4Modal").on("click", ".modal-background", function () {
+   $("#plant4Modal").removeClass("is-active");  
+});
+
+//5th circle image
+$("#plant5").on("click", function () {
+   $("#plant5Modal").addClass("is-active");
+});
+
+$("#plant5Modal").on("click", "#close", function () {
+   $("#plant5Modal").removeClass("is-active");  
+});
+
+$("#plant5Modal").on("click", ".modal-background", function () {
+   $("#plant5Modal").removeClass("is-active");  
+});
+
+
+
+
+
+
    // direct to new highlight modal when user clicks on + button ========================
    $("#addNewHighlight").on("click", function () {
        $("#newHighlightModal").addClass("is-active");
@@ -51,4 +122,6 @@ $(document).ready(function () {
        $(".navbar-menu").toggleClass("is-active");
    });
 });
+
+
 
