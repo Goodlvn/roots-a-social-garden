2261b6e9bc117ca36fcf638204c7125df27f400b
unknown <christy.g.lee@gmail.com>
2020-11-02 19:11:30 -0800

    added .show and .hide model function

--- public/js/plantFinder.js
+++ public/js/plantFinder.js
@@ -1,6 +1,5 @@
 $("#modal").hide();
 
-
 $("#plantFinderBtn").on("click", function(event){
 event.preventDefault();
 $("#instructions").empty();
@@ -9,19 +8,29 @@ var plantSearch = $("#searchPlant").val().trim();
 
 $.get("/api/plant_data/" + plantSearch, (response) => {
 
-  // console.log(data);
-  
-  var plantName = response.data[0].common_name;
-  var familyName = response.data[0].family_common_name;
-  var scientificName = response.data[0].scientific_name;
-  var image = response.data[0].image_url;
-//   var x = response.data[0]
-  $("#cardImage").attr("src", image)
-  $("#plantName").text("Plant Name: " + plantName);
-  $("#familyName").text("Family Common Name: " + familyName);
-  $("#scientificName").text("Scientific Name: " + scientificName);
+  console.log(response);
+  if(response.data.length === 0) {
+    $("#modal").show();
+    $("#instructions").empty();
+  } 
+  else if (response.data[0].common_name === null){
+    $("#modal").show();
+    $("#instructions").empty();
 
-} );
+  } else {
+    $("#modal").hide();
+    var plantName = response.data[0].common_name;
+    var familyName = response.data[0].family_common_name;
+    var scientificName = response.data[0].scientific_name;
+    var image = response.data[0].image_url;
+
+    $("#cardImage").attr("src", image)
+    $("#plantName").text("Plant Name: " + plantName);
+    $("#familyName").text("Family Common Name: " + familyName);
+    $("#scientificName").text("Scientific Name: " + scientificName);
+  }
+
+});
 
 
-})
+});
